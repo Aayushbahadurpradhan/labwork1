44ce6330e88cab390bf1fa6227693fa3fadc153f
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2021-11-29 13:05:32 +0545

    initial commit

---
+++ Lab/lab2.py
@@ -0,0 +1,4 @@
+h = int(input("enter height?"))
+b = int(input("enter base?"))
+area = 1/2*b*h
+print(f"the area of the triangle is {area} cm\u00b2")
---
+++ Lab/lab3.py
@@ -0,0 +1,3 @@
+l =int(input("enter the length?"))
+V = l**3
+print(f"the volume of the cube is {V} cm\u00b3")
