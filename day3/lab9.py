ad1be75fe60704b7b989ff231b94c686b8386e70
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2021-12-06 12:21:48 +0545

    initial commit

--- day3/lab9.py
+++ day3/lab9.py
@@ -3,15 +3,15 @@
 # for consecutive 3 times and if the limit exceeds than print "Attempt finished".
 username=input("enter your username:")
 password=input("enter your password:")
+
 for a in range (0,3):
-    print("enter your credentials")
     username1=input("enter your username:")
     password1=input("enter your password:")
-    if username==username1 and password==password1:
+    if username == username1 and password==password1:
         print("you are successfully logged.")
         break
     else:
-        if(username!=username1 or password!=password1):
+        if( username!=username1 or password!=password1):
             print("invalid credentials")
 else:
-    print("3attemt fnished")
+    print("3 attemt fnished")
